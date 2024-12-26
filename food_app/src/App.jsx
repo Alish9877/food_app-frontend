59e8f33fd0b8efb3f538e56fd7b0b87f6b2485fc
alish9877 <alshaikhali302@gmail.com>
2024-12-26 15:10:19 +0300

    auth

--- food_app/src/App.jsx
+++ food_app/src/App.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router'
+import Nav from './components/Nav'
+import Register from '../pages/Register'
+import SignIn from '../pages/SignIn'
+import Feed from '../pages/Feed'
+import Home from '../pages/Home'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    localStorage.clear()
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Nav
+        user={user}
+        handleLogOut={handleLogOut}
+      />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="./signin" element={<SignIn />} />
+          <Route path="./register" element={<Register />} />
+          <Route path="./feed" element={<Feed />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
