00f775b600b2f9dfebc72569d6b519b340aea4a7
alish9877 <alshaikhali302@gmail.com>
2024-12-26 15:21:03 +0300

    Auth done

--- food_app/src/App.jsx
+++ food_app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Route, Routes } from 'react-router'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Nav from './components/Nav'
 import Register from '../pages/Register'
 import SignIn from '../pages/SignIn'
@@ -11,25 +11,27 @@ const App = () => {
   const [user, setUser] = useState(null)
 
   const handleLogOut = () => {
-    //Reset all auth related state and clear localStorage
+    // Reset all auth related state and clear localStorage
     setUser(null)
     localStorage.clear()
   }
 
   return (
     <div className="App">
-      <Nav
-        user={user}
-        handleLogOut={handleLogOut}
-      />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="./signin" element={<SignIn />} />
-          <Route path="./register" element={<Register />} />
-          <Route path="./feed" element={<Feed />} />
-        </Routes>
-      </main>
+      <BrowserRouter>
+        <Nav
+          user={user}
+          handleLogOut={handleLogOut}
+        />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/feed" element={<Feed />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
