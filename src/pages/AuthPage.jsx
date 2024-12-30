bf0b10a272dd3222491875a443b7197e140f6ec6
unknown <kqyz12@gmail.com>
2024-12-30 09:59:14 +0300

    fix conflits

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -18,19 +18,3 @@ const AuthPage = () => {
 
 export default AuthPage;
 
-import { useState } from "react"
-
-const AuthPage = () => {
-const [isLogin , setIsLogin] = useState()
-
-return (
-  <div  className="auth-page">
-    <h1>{isLogin? 'Login' : 'Register'}</h1>
-    {/* add from components */}
-    <button onClick={() => setIsLogin (!isLogin)}>Switch to {isLogin? 'Register' : 'Login'}</button>
-  </div>
-)
-}
-
-export default AuthPage
-
